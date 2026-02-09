0f07955090fe1dcd56503fca6adb6f3014f89ad3
Mariia Vovk <mariia.vovk.contact@gmail.com>
2026-02-09 10:38:23 +0200

    Integrated the script into Substance Designer UI. Packaged for installation.

--- random_frame_colors/__init__.py
+++ random_frame_colors/__init__.py
@@ -3,7 +3,7 @@
 # random_frame_colors
 #
 # Offers an alternative to a built-in "Add frame" functionality in Substance Designer. When used,
-# newly created frames are automatically assigned a random color, instead of the default blue.
+# newly created frames are automatically assigned a random, visually distinct color instead of the default blue.
 #
 # Created by Mariia Vovk - https://github.com/vovkmariia - February 2026
 
@@ -16,9 +16,6 @@ import weakref
 
 from functools import partial
 from collections import OrderedDict
-
-import random
-import colors
 
 from sd.tools import io
 from sd.tools import graphlayout
@@ -37,66 +34,177 @@ from sd.api.sdapplication import SDApplicationPath
 
 from PySide6 import QtCore, QtGui, QtWidgets, QtSvg
 
+# importing libraries required by the custom function of the plugin
+import random
+from .colors import *
 
-# getting the application and UI manager object
-ctx = sd.getContext()
-app = ctx.getSDApplication()
-uiMgr = app.getQtForPythonUIMgr()
 
-# getting the current graph.
-sdGraph = uiMgr.getCurrentGraph()
+#------function for loading UI icons from SVG files (from factory plugin 'node_align_tools')--------------------------
+
+def loadSvgIcon(iconName, size):
+    currentDir = os.path.dirname(__file__)
+    iconFile = os.path.abspath(os.path.join(currentDir, iconName + '.svg'))
+
+    svgRenderer = QtSvg.QSvgRenderer(iconFile)
+    if svgRenderer.isValid():
+        pixmap = QtGui.QPixmap(QtCore.QSize(size, size))
+
+        if not pixmap.isNull():
+            pixmap.fill(QtCore.Qt.transparent)
+            painter = QtGui.QPainter(pixmap)
+            svgRenderer.render(painter)
+            painter.end()
+
+        return QtGui.QIcon(pixmap)
+
+    return None
 
 
 #------------ START MAIN CUSTOM FUNCTIONS ---------------------------------------------------------------------------
 
-with SDHistoryUtils.UndoGroup("Random Frame Colors"):
+class randomColorFrameToolbar(QtWidgets.QToolBar):
+    __toolbarList = {}
 
-    # getting selected nodes (as SDSBSCompNode objects)
-    selected_nodes = uiMgr.getCurrentGraphSelectedNodes() # returns selected nodes in an SDArray
+    def create_random_color_frame(self):
+        with SDHistoryUtils.UndoGroup("Random Frame Colors"):
+
+            # getting the application and UI manager object
+            ctx = sd.getContext()
+            app = ctx.getSDApplication()
+            uiMgr = app.getQtForPythonUIMgr()
+
+            # getting the current graph
+            sdGraph = uiMgr.getCurrentGraph()
+
+            # getting selected nodes (as SDSBSCompNode objects)
+            selected_nodes = uiMgr.getCurrentGraphSelectedNodes()  # returns selected nodes in an SDArray
+            if not selected_nodes:
+                print("No nodes selected.")
+                return
+
+            # calculating a bounding box for the frame
+            min_x = min(node.getPosition()[0] for node in selected_nodes)
+            # .getPosition() returns an instance of a
+            # float2 class. by accessing attributes
+            # with [0] and [1] we get a float for performing
+            # calculations
+            min_y = min(node.getPosition()[1] for node in selected_nodes)
+            max_x = max(node.getPosition()[0] for node in selected_nodes)
+            max_y = max(node.getPosition()[1] for node in selected_nodes)
+
+            padding = 80
+
+            # preparing the floats
+            x_pos = (min_x - padding)
+            y_pos = (min_y - padding)
+            x_size = (max_x - min_x) + padding * 2
+            y_size = (max_y - min_y) + padding * 2
+
+            # creating a frame
+            frame = SDGraphObjectFrame.sNew(sdGraph)
+
+            # assigning our floats to float2 x and y attributes
+            frame_pos = frame.getPosition()
+            frame_pos.x = x_pos
+            frame_pos.y = y_pos
+            frame_size = frame.getSize()
+            frame_size.x = x_size
+            frame_size.y = y_size
+
+            print(f"Number of available unique colors: {len(self.temp_color_options)}")
+
+            if len(self.temp_color_options) == 0:
+                self.temp_color_options = color_options.copy()
+                print("All colors are used. Resetting the dictionary.")
+
+            # getting a random color from a dictionary
+            random_key = random.choice(list(self.temp_color_options.keys()))
+            frame_colors = self.temp_color_options[random_key]
+            R = frame_colors[0]
+            G = frame_colors[1]
+            B = frame_colors[2]
+            A = frame_colors[3]
+            value = self.temp_color_options.pop(random_key)
+            print(f"Color generated: {random_key}")
+
+            # applying the layout of the frame
+            frame.setPosition(frame_pos)
+            frame.setSize(frame_size)
+            frame.setTitle("Frame")
+            frame.setColor(ColorRGBA(R, G, B, A))
+
+    def __init__(self, graphViewID, uiMgr):
+        super(randomColorFrameToolbar, self).__init__(parent=uiMgr.getMainWindow())
+        # creating a dictionary of available colors
+        self.temp_color_options = color_options.copy()
+
+        self.setObjectName("vovkmariia.random_frame_colors_toolbar")
+
+        self.__graphViewID = graphViewID
+        self.__uiMgr = uiMgr
+
+        act = self.addAction(loadSvgIcon("color_frame", 24), "Color_Frame")
+        act.setToolTip(self.tr("Create Frame with Random Color"))
+        act.triggered.connect(self.create_random_color_frame)
 
 
-    # calculating a bounding box for the frame
-    min_x = min(node.getPosition()[0] for node in selected_nodes) # .getPosition() returns an instance of a
-                                                                  # float2 class. by accessing attributes
-                                                                  # with [0] and [1] we get a float for performing
-                                                                  # calculations
-    min_y = min(node.getPosition()[1] for node in selected_nodes)
-    max_x = max(node.getPosition()[0] for node in selected_nodes)
-    max_y = max(node.getPosition()[1] for node in selected_nodes)
+        self.__toolbarList[graphViewID] = weakref.ref(self)
+        self.destroyed.connect(partial(randomColorFrameToolbar.__onToolbarDeleted, graphViewID=graphViewID))
 
-    padding = 80
+    def tooltip(self):
+        return self.tr("Create Frame with Random Color")
 
-    # preparing the floats
-    x_pos = (min_x - padding)
-    y_pos = (min_y - padding)
-    x_size = (max_x - min_x) + padding * 2
-    y_size = (max_y - min_y) + padding * 2
 
-    # creating a frame
-    frame = SDGraphObjectFrame.sNew(sdGraph)
+    # (from factory plugin 'node_align_tools')
+    @classmethod
+    def __onToolbarDeleted(cls, graphViewID):
+        del cls.__toolbarList[graphViewID]
 
-    # assigning our floats to float2 x and y attributes
-    frame_pos = frame.getPosition()
-    frame_pos.x = x_pos
-    frame_pos.y = y_pos
-    frame_size = frame.getSize()
-    frame_size.x = x_size
-    frame_size.y = y_size
-
-    # getting a random color from a dictionary
-    frame_colors = random.choice(list(colors.color_options.values()))
-    R = frame_colors[0]
-    G = frame_colors[1]
-    B = frame_colors[2]
-    A = frame_colors[3]
-
-    # applying the layout of the frame
-    frame.setPosition(frame_pos)
-    frame.setSize(frame_size)
-    frame.setTitle("Frame")
-    frame.setColor(ColorRGBA(R, G, B, A))
-
-    sdGraph.setSelectedGraphObjects([frame])
+    # (from factory plugin 'node_align_tools')
+    @classmethod
+    def removeAllToolbars(cls):
+        for toolbar in cls.__toolbarList.values():
+            if toolbar():
+                toolbar().deleteLater()
 
 #------------ END MAIN CUSTOM FUNCTIONS --------------------------------------------------------------------------
 
+# (from factory plugin 'node_align_tools')
+def onNewGraphViewCreated(graphViewID, uiMgr):
+    # Ignore graph types not supported by the Python API.
+    if not uiMgr.getCurrentGraph():
+        return
+
+    toolbar = randomColorFrameToolbar(graphViewID, uiMgr)
+    uiMgr.addToolbarToGraphView(
+        graphViewID,
+        toolbar,
+        icon = loadSvgIcon("color_frame", 24),
+        tooltip = toolbar.tooltip())
+
+graphViewCreatedCallbackID = 0
+
+# (from factory plugin 'node_align_tools')
+def initializeSDPlugin():
+
+    # Get the application and UI manager object.
+    ctx = sd.getContext()
+    app = ctx.getSDApplication()
+    uiMgr = app.getQtForPythonUIMgr()
+
+    if uiMgr:
+        global graphViewCreatedCallbackID
+        graphViewCreatedCallbackID = uiMgr.registerGraphViewCreatedCallback(
+            partial(onNewGraphViewCreated, uiMgr=uiMgr))
+
+
+# (from factory plugin 'node_align_tools')
+def uninitializeSDPlugin():
+    ctx = sd.getContext()
+    app = ctx.getSDApplication()
+    uiMgr = app.getQtForPythonUIMgr()
+
+    if uiMgr:
+        global graphViewCreatedCallbackID
+        uiMgr.unregisterCallback(graphViewCreatedCallbackID)
+        randomColorFrameToolbar.removeAllToolbars()
--- random_frame_colors/colors.py
+++ random_frame_colors/colors.py
@@ -1,28 +1,20 @@
-default_alpha = 0.20
+default_alpha = 0.25
 
 color_options = {
-        'violet':      [0.41569, 0.00000, 1.00000, default_alpha],  # 6A00FF
-        'magenta':     [1.00000, 0.00000, 1.00000, default_alpha],  # FF00FF
-        'crimsonred':  [1.00000, 0.00000, 0.25098, default_alpha],  # FF0040
-        'orange':      [1.00000, 0.58431, 0.00000, default_alpha],  # FF9500
-        'yellow':      [1.00000, 1.00000, 0.00000, default_alpha],  # FFFF00
-        'yellowgreen': [0.66667, 1.00000, 0.00000, default_alpha],  # AAFF00
-        'springgreen': [0.00000, 1.00000, 0.08235, default_alpha],  # 00FF15
-        'cyan':        [0.00000, 1.00000, 1.00000, default_alpha],  # 00FFFF
-        'azureblue':   [0.00000, 0.58431, 1.00000, default_alpha],  # 0095FF
-        'electricpurple': [0.60000, 0.00000, 1.00000, default_alpha],  # 9900FF
-
-        'neonpink':       [1.00000, 0.00000, 0.60000, default_alpha],  # FF0099
-        'scarlet':        [1.00000, 0.14118, 0.00000, default_alpha],  # FF2400
-        'tangerine':      [1.00000, 0.47059, 0.00000, default_alpha],  # FF7800
-        'amber':          [1.00000, 0.75098, 0.00000, default_alpha],  # FFBF00
-        'chartreuse':     [0.49804, 1.00000, 0.00000, default_alpha],  # 7FFF00
-        'neongreen':      [0.22353, 1.00000, 0.07843, default_alpha],  # 39FF14
-        'turquoise':      [0.00000, 0.96078, 0.80000, default_alpha],  # 00F5CC
-        'brightcyan':     [0.00000, 0.87843, 1.00000, default_alpha],  # 00E0FF
-        'dodgerblue':     [0.11765, 0.56471, 1.00000, default_alpha],  # 1E90FF
-        'ultramarine':    [0.07059, 0.03922, 0.56078, default_alpha],  # 120A8F
-
+        'violet':      [0.41569, 0.00000, 1.00000, default_alpha],
+        'crimsonred':  [0.80000, 0.00000, 0.05098, default_alpha],
+        'yellow':      [1.00000, 1.00000, 0.00000, default_alpha],
+        'yellowgreen': [0.66667, 1.00000, 0.00000, 0.4],
+        'springgreen': [0.00000, 1.00000, 0.08235, default_alpha],
+        'cyan':        [0.00000, 1.00000, 1.00000, default_alpha],
+        'azureblue':   [0.00000, 0.28431, 0.70000, 0.6],
+        'neonpink':       [1.00000, 0.00000, 0.60000, default_alpha],
+        'darkorange':     [1.00000, 0.54902, 0.00000, 0.4],
+        'amber':          [0.70000, 0.45098, 0.00000, default_alpha],
+        'khaki':          [0.94118, 0.90196, 0.54902, 0.4],
+        'navy':           [0.00000, 0.00000, 0.50000, default_alpha],
+        'darkslategray':  [0.18431, 0.30980, 0.30980, 0.6],
+        'lightsalmon':    [1.00000, 0.62745, 0.47843, default_alpha],
         'default':        [0.10196079, 0.5529412, 1.0, 0.2509804], # Factory default color
 
     }
